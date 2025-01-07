979397ce147974f1e8c688ce588809f9583bc48f
Osk <52168556+O-S-K@users.noreply.github.com>
2025-01-07 17:53:15 +0700

    merge

--- Runtime/Common/CanvasScaleHandler.cs
+++ Runtime/Common/CanvasScaleHandler.cs
@@ -6,38 +6,40 @@ namespace OSK
     public class CanvasScaleHandler : MonoBehaviour
     {
         [SerializeField] private Camera camera;
-        [SerializeField] private UnityEngine.UI.CanvasScaler canvasScaler;
+        [SerializeField] private Canvas canvas;
         [SerializeField] private bool isPortrait;
 
         private void Awake()
-        {
+        { 
             float newRatio = (float)Screen.width / Screen.height;
 
             if (camera == null)
                 camera = GetComponent<Camera>();
-            if (canvasScaler == null)
-                canvasScaler = GetComponent<UnityEngine.UI.CanvasScaler>();
-
+ 
             SetupCanvasScaler(newRatio);
         }
+ 
 
-        private void OnDrawGizmos()
+#if UNITY_EDITOR
+        private void OnValidate()
         {
-            if (Application.isPlaying) return;
-            if (camera != null && canvasScaler != null)
-            {
-                //float currentRatio = isPortrait ? 1080f / 1920 : 1920f / 1080;
+            if (Application.isPlaying) 
+                return;
+            if (camera != null && canvas != null)
+            { 
                 float newRatio = (float)camera.pixelWidth / camera.pixelHeight;
                 SetupCanvasScaler(newRatio);
-                
+                // float currentRatio = isPortrait ? 1080f / 1920 : 1920f / 1080;
                 // float scaleFactor = canvasScaler.referenceResolution.x / camera.pixelWidth;
-                // Debug.Log("canvas Scaler Factor X: " + scaleFactor);
+                // Logg.Log("canvas Scaler Factor X: " + scaleFactor);
             }
         }
+#endif
+
 
         private void SetupCanvasScaler(float ratio)
         {
-            canvasScaler.matchWidthOrHeight = ratio > 0.65f ? 1 : 0;
+            canvas.GetComponent<CanvasScaler>().matchWidthOrHeight = ratio > 0.65f ? 1 : 0;
         }
     }
 }
